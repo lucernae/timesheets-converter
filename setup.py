264a0f05034ca026d502269e3cfc0f1918025071
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-02-09 22:24:54 +0700

    Added tox

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ from distutils.core import setup
 setup(
     name='timesheets-converter',
     version='0.1-dev',
+    install_requires=[
+    'future>=0',
+    ],
     packages=['timesheets', ],
 )
--- timesheets/tests.py
+++ timesheets/tests.py
@@ -8,21 +8,30 @@ from timesheets.format.toggl import TogglTimeRecord, TogglTagsTimeRecord
 from timesheets.timesheet import TimeSheets
 
 
+def data_path(*args):
+    cur_dir = os.path.dirname(__file__)
+    return os.path.join(cur_dir, *args)
+
+
 class TestSageOne(unittest.TestCase):
 
     def test_load_csv(self):
         ts = TimeSheets()
         ts.time_record_type = SageOneTimeRecord
-        ts.load_csv('test/timesheets-sageone.csv')
+        ts.load_csv(
+            data_path('test/timesheets-sageone.csv'))
         self.assertEqual(4, len(ts.records))
 
     def test_dump_csv(self):
         ts = TimeSheets()
         ts.time_record_type = SageOneTimeRecord
-        ts.load_csv('test/timesheets-sageone.csv')
+        ts.load_csv(
+            data_path('test/timesheets-sageone.csv'))
 
-        ts.dump_csv('test/dumped-sageone.csv')
-        self.assertTrue(os.path.exists('test/dumped-sageone.csv'))
+        ts.dump_csv(
+            data_path('test/dumped-sageone.csv'))
+        self.assertTrue(os.path.exists(
+            data_path('test/dumped-sageone.csv')))
 
 
 class TestHarvest(unittest.TestCase):
@@ -30,30 +39,39 @@ class TestHarvest(unittest.TestCase):
     def test_load_csv(self):
         ts = TimeSheets()
         ts.time_record_type = HarvestTimeRecord
-        ts.load_csv('test/timesheets-harvest.csv')
+        ts.load_csv(
+            data_path('test/timesheets-harvest.csv'))
         self.assertEqual(2, len(ts.records))
 
     def test_dump_csv(self):
         ts = TimeSheets()
         ts.time_record_type = HarvestTimeRecord
-        ts.load_csv('test/timesheets-harvest.csv')
+        ts.load_csv(
+            data_path('test/timesheets-harvest.csv'))
 
-        ts.dump_csv('test/dumped-harvest.csv', one_line=True)
-        self.assertTrue(os.path.exists('test/dumped-harvest.csv'))
+        ts.dump_csv(
+            data_path('test/dumped-harvest.csv'),
+            one_line=True)
+        self.assertTrue(os.path.exists(
+            data_path('test/dumped-harvest.csv')))
 
         # multiple line a task should become one line
-        ts.load_csv('test/dumped-harvest.csv')
+        ts.load_csv(
+            data_path('test/dumped-harvest.csv'))
         self.assertEqual(1, len(ts.records[1].notes.splitlines()))
 
     def test_convert_to_sageone(self):
         ts = TimeSheets()
         ts.time_record_type = HarvestTimeRecord
-        ts.load_csv('test/timesheets-harvest.csv')
+        ts.load_csv(
+            data_path('test/timesheets-harvest.csv'))
         self.assertEqual(2, len(ts.records))
 
         ts.dump_csv(
-            'test/dumped-harvest-sageone.csv', target_type=SageOneTimeRecord)
-        self.assertTrue(os.path.exists('test/dumped-harvest-sageone.csv'))
+            data_path('test/dumped-harvest-sageone.csv'),
+            target_type=SageOneTimeRecord)
+        self.assertTrue(os.path.exists(
+            data_path('test/dumped-harvest-sageone.csv')))
 
 
 class TestToggl(unittest.TestCase):
@@ -61,19 +79,25 @@ class TestToggl(unittest.TestCase):
     def test_load_csv(self):
         ts = TimeSheets()
         ts.time_record_type = TogglTimeRecord
-        ts.load_csv('test/timesheets-toggl.csv')
+        ts.load_csv(
+            data_path('test/timesheets-toggl.csv'))
         self.assertEqual(5, len(ts.records))
         self.assertEqual(0.37166666666666665, ts.records[0].duration)
 
     def test_dump_csv(self):
         ts = TimeSheets()
         ts.time_record_type = TogglTimeRecord
-        ts.load_csv('test/timesheets-toggl.csv')
+        ts.load_csv(
+            data_path('test/timesheets-toggl.csv'))
 
-        ts.dump_csv('test/dumped-toggl.csv', aggregate=True)
-        self.assertTrue(os.path.exists('test/dumped-toggl.csv'))
+        ts.dump_csv(
+            data_path('test/dumped-toggl.csv'),
+            aggregate=True)
+        self.assertTrue(os.path.exists(
+            data_path('test/dumped-toggl.csv')))
 
-        ts.load_csv('test/dumped-toggl.csv')
+        ts.load_csv(
+            data_path('test/dumped-toggl.csv'))
         # Test that same task is merged
         self.assertEqual(4, len(ts.records))
         self.assertEqual(5.0, ts.records[3].duration)
@@ -81,15 +105,19 @@ class TestToggl(unittest.TestCase):
     def test_convert_to_sageone(self):
         ts = TimeSheets()
         ts.time_record_type = TogglTimeRecord
-        ts.load_csv('test/timesheets-toggl.csv')
+        ts.load_csv(
+            data_path('test/timesheets-toggl.csv'))
         self.assertEqual(5, len(ts.records))
 
         ts.dump_csv(
-            'test/dumped-toggl-sageone.csv', target_type=SageOneTimeRecord)
-        self.assertTrue(os.path.exists('test/dumped-toggl-sageone.csv'))
+            data_path('test/dumped-toggl-sageone.csv'),
+            target_type=SageOneTimeRecord)
+        self.assertTrue(os.path.exists(
+            data_path('test/dumped-toggl-sageone.csv')))
 
         ts.load_csv(
-            'test/dumped-toggl-sageone.csv', target_type=SageOneTimeRecord)
+            data_path('test/dumped-toggl-sageone.csv'),
+            target_type=SageOneTimeRecord)
 
         self.assertEqual('11/06/2018', ts.records[0].date)
 
@@ -99,7 +127,8 @@ class TestTogglTags(unittest.TestCase):
     def test_load_csv(self):
         ts = TimeSheets()
         ts.time_record_type = TogglTagsTimeRecord
-        ts.load_csv('test/timesheets-toggl.csv')
+        ts.load_csv(
+            data_path('test/timesheets-toggl.csv'))
         self.assertEqual(5, len(ts.records))
         self.assertEqual(0.37166666666666665, ts.records[0].duration)
         self.assertTrue(ts.records[0].task)
@@ -107,12 +136,17 @@ class TestTogglTags(unittest.TestCase):
     def test_dump_csv(self):
         ts = TimeSheets()
         ts.time_record_type = TogglTagsTimeRecord
-        ts.load_csv('test/timesheets-toggl.csv')
+        ts.load_csv(
+            data_path('test/timesheets-toggl.csv'))
 
-        ts.dump_csv('test/dumped-toggl-tags.csv', aggregate=True)
-        self.assertTrue(os.path.exists('test/dumped-toggl-tags.csv'))
+        ts.dump_csv(
+            data_path('test/dumped-toggl-tags.csv'),
+            aggregate=True)
+        self.assertTrue(os.path.exists(
+            data_path('test/dumped-toggl-tags.csv')))
 
-        ts.load_csv('test/dumped-toggl-tags.csv')
+        ts.load_csv(
+            data_path('test/dumped-toggl-tags.csv'))
         # Test that same task is merged
         self.assertEqual(4, len(ts.records))
         self.assertEqual(5.0, ts.records[3].duration)
@@ -120,16 +154,18 @@ class TestTogglTags(unittest.TestCase):
     def test_convert_to_sageone(self):
         ts = TimeSheets()
         ts.time_record_type = TogglTagsTimeRecord
-        ts.load_csv('test/timesheets-toggl.csv')
+        ts.load_csv(
+            data_path('test/timesheets-toggl.csv'))
         self.assertEqual(5, len(ts.records))
 
         ts.dump_csv(
-            'test/dumped-toggl-tags-sageone.csv',
+            data_path('test/dumped-toggl-tags-sageone.csv'),
             target_type=SageOneTimeRecord)
-        self.assertTrue(os.path.exists('test/dumped-toggl-tags-sageone.csv'))
+        self.assertTrue(os.path.exists(
+            data_path('test/dumped-toggl-tags-sageone.csv')))
 
         ts.load_csv(
-            'test/dumped-toggl-tags-sageone.csv',
+            data_path('test/dumped-toggl-tags-sageone.csv'),
             target_type=SageOneTimeRecord)
 
         self.assertEqual('11/06/2018', ts.records[0].date)
